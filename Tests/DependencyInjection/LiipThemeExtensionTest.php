f0eb47eab603734e95bb00c1a0c1af0030a1378e
Tobias Ebnöther <ebi@gorn.ch>
2011-03-25 12:02:47 +0100

    Added tests for DI creation

---
+++ Tests/DependencyInjection/LiipThemeExtensionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Liip\ThemeBundle\Tests\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Parser;
+use Liip\ThemeBundle\DependencyInjection\LiipThemeExtension;
+
+class LiipThemeExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Liip\ThemeBundle\DependencyInjection\LiipThemeExtension::load
+     * @expectedException \RuntimeException
+     */
+    public function testLoadFailure()
+    {
+        $loader = new LiipThemeExtension();
+        $config = $this->getConfig();
+        unset($config['themes']);
+        $loader->load(array($config), new ContainerBuilder());
+    }
+    
+    /**
+     * @covers Liip\ThemeBundle\DependencyInjection\LiipThemeExtension::load
+     */
+    public function testLoad()
+    {
+        $container = new ContainerBuilder();
+        $loader = new LiipThemeExtension();
+        $config = $this->getConfig();
+        $loader->load(array($config), $container);
+        $this->assertEquals(array('web', 'tablet', 'mobile'), $container->getParameter('liip_theme.themes'));
+        $this->assertEquals('tablet', $container->getParameter('liip_theme.activeTheme'));
+    }
+    
+    protected function getConfig()
+    {
+        $yaml = <<<EOF
+themes: ['web', 'tablet', 'mobile']
+activeTheme: 'tablet'
+EOF;
+        $parser = new Parser();
+
+        return $parser->parse($yaml);
+    }
+
+}
