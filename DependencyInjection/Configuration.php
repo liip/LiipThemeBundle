3e290c229f5ba55d9ef56ea2b14ea9b2ee600987
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-05-27 15:23:00 +0200

    Fixed wrong method for defaulting to null

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -32,7 +32,7 @@ class Configuration
                     ->prototype('scalar')
                 ->end()
             ->end()
-            ->scalarNode('active_theme')->defaultValue(null)->end()
+            ->scalarNode('active_theme')->defaultNull()->end()
         ->end();
         return $treeBuilder->buildTree();
     }
