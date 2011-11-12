f5b3eeb60396a00e8fa90bdb5a56f44e98435cbc
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-12 01:05:14 +0100

    travis-ci

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,22 +1,7 @@
 <?php
 
-if (! file_exists($file = $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php')) {
-    throw new \Exception('Modify the phpunit.xml to match your Symfony path.');
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
 }
-
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Liip\\ThemeBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
---
+++ vendor/vendors.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+if (!is_dir($vendorDir = __DIR__)) {
+    mkdir($vendorDir, 0777, true);
+}
+
+$deps = array(
+    array('symfony', 'http://github.com/symfony/symfony', 'v2.0.5'),
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    if ($rev) {
+        system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+    }
+}
