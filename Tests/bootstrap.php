8b51ad3833f0ee27d3fcd5327f6bedfcc4b4dfcc
lsmith77 <smith@pooteeweet.org>
2012-01-10 19:59:32 +0100

    use composer for travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,19 @@
 <?php
 
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (file_exists($file = __DIR__ . '/../vendor/.composer/autoload.php')) {
+    $autoload = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
 }
+
+spl_autoload_register(function($class)
+{
+    if (0 === strpos($class, 'Liip\\ThemeBundle\\')) {
+        $path = __DIR__ . '/../' . implode('/', array_slice(explode('\\', $class), 2)) . '.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
